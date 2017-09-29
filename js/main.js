6e70e40e2d18f41f44dde95966d02d1d719a8a46
masonp <masonp@seattleu.edu>
2017-09-28 20:21:55 -0400

    added main.js

--- js/main.js
+++ js/main.js
@@ -13,67 +13,46 @@
 
 let honorific = prompt('Please enter an honorific title (e.g. Mister or Colonel).');
 
-// "authorName" prompt to get a name from user
 let authorName = prompt('What is your full name?');
 
-// "adjective1" to get a word used to describe something
 let adjective1 = prompt('Give me a word to describe something');
 
-// "vehicle" prompt for the name of a vehicle
 let vehicle = prompt('What is your favorite vehicle?');
 
-// "vehiclePart" for a part of the vehicle just named
 let vehiclePart = prompt('A part of the vehicle you just named');
 
-// "parking" for a place to keep said vehicle
 let parking = prompt('Name a place that you might keep this vehicle');
 
-// "precious" prompt for precious object
 let precious = prompt('Name a precious object');
 
-// "noiseMaker" prompt for something that makes noise
 let noiseMaker = prompt('Name something that makes noise');
 
-// "crowdBehavior" for a group activity of some kind
 let crowdBehavior = prompt('Name something a group of people do together');
 
-// "adjective2" prompt for a descriptive word
 let adjective2 = prompt('What is another word to describe something');
 
-// "adjective3" for another descriptive word
 let adjective3 = prompt('Give me another word to describe something');
 
-// "color" for the name of a color
 let color = prompt('What is your favorite color?');
 
-// "familyMember" for the name of a family relationship (e.g. mother, father, brother etc.)
 let familyMember = prompt('Name a family relationship (e.g mother, father, brother, etc.)');
 
-// "organ" for the name of an organ in the body (e.g. heart, liver, brain, etc.).
 let organ = prompt('Name an organ in the body (e.g heart, liver, brain)');
 
-// "bodyPart1" for the name of a part of the body
 let bodyPart1 = prompt('Name a part of the body');
 
-// "bodyPart2" for the name of another part of the body
 let bodyPart2 = prompt('Name another body part');
 
-// "bodyPart3" for a name of a third part of the body
 let bodyPart3 = prompt('Name a third body part');
 
-// "thing" prompt to name an object
 let thing = prompt('Name an object');
 
-// "thing2" for an object you might give somebody you love
 let thing2 = prompt('Name an object you might give to someone you love');
 
-// "thing3" for the name of an object to give to someone you like
 let thing3 = prompt('Name an object you might give to someone you like');
 
-// "adjective4" for a word to describe a vehicle
 let adjective4 = prompt('A word used to describe a vehicle');
 
-// "adjective5" for a word used to describe a vehicle
 let adjective5 = prompt('What is another word used to describe a vehicle?');
 
 
@@ -83,13 +62,12 @@ let adjective5 = prompt('What is another word used to describe a vehicle?');
 // The poem we are using has section numbers. Let's allow the user to
 // generate their own.
 //
-//  "userNumber" prompt for the user's lucky number 
 let userNumber = prompt('What is your lucky number?');
 
-// "number2" to change the number submitted
+// let number2 divide userNumber by 2
 let number2 = userNumber / 2;
 
-// "number3" to further change the number submitted
+// let number3 divide userNumber by number 2 
 let number3 = userNumber / number2 
 // DO NOT EDIT BELOW THIS LINE /////////////////////////////////////////
 //
